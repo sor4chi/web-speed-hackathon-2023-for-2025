d18386884d46bc5d91058ba5b8ba658a3eb5ab9e
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2025-02-11 02:05:52 +0900

    perf: nosync

--- src/client/utils/apollo_client.ts
+++ src/client/utils/apollo_client.ts
@@ -1,39 +1,7 @@
-import type { HttpOptions } from '@apollo/client';
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
-
-const syncXhr: HttpOptions['fetch'] = (uri, options) => {
-  return new Promise((resolve, reject) => {
-    const method = options?.method;
-    if (method === undefined) {
-      return reject();
-    }
-
-    const body = options?.body;
-    if (body instanceof ReadableStream) {
-      return reject();
-    }
-
-    const request = new XMLHttpRequest();
-    request.open(method, uri.toString(), false);
-    request.setRequestHeader('content-type', 'application/json');
-    request.onload = () => {
-      if (request.status >= 200 && request.status < 300) {
-        return resolve(new Response(request.response));
-      }
-
-      reject();
-    };
-    request.onerror = reject;
-
-    request.send(body);
-  });
-};
-
-const link = new HttpLink({ fetch: syncXhr });
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
-  connectToDevTools: true,
   defaultOptions: {
     mutate: {
       fetchPolicy: 'network-only',
@@ -45,7 +13,6 @@ export const apolloClient = new ApolloClient({
       fetchPolicy: 'network-only',
     },
   },
-  link,
   queryDeduplication: false,
   uri: '/graphql',
 });
