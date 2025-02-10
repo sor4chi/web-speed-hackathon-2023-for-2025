3f70802b9ce1ddd5a6a72d45600fffdacd457e4c
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2025-02-11 01:05:39 +0900

    perf: optimize product small image

--- optimize.js
+++ optimize.js
@@ -28,4 +28,30 @@ await Promise.all(
       .webp({ quality: 75 })
       .toFile(path.replace('.jpg', '-1024x576.webp'));
   }),
+  // product small images
+  (
+    await glob('./public/images/products/**/*.jpg')
+  ).map(async (path) => {
+    await sharp(path)
+      .resize({
+        fit: 'cover',
+        height: 40,
+        width: 40,
+      })
+      .webp({ quality: 75 })
+      .toFile(path.replace('.jpg', '-40x40.webp'));
+  }),
+  // product video thumbnails
+  (
+    await glob('./public/videos/*.png')
+  ).map(async (path) => {
+    await sharp(path)
+      .resize({
+        fit: 'cover',
+        height: 40,
+        width: 40,
+      })
+      .webp({ quality: 75 })
+      .toFile(path.replace('.png', '-40x40.webp'));
+  }),
 );
