ca27b93251fb69c0d5bd354145c6592005bc5aa3
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2025-02-10 23:39:28 +0900

    perf: import specific icons

--- src/client/components/foundation/Icon/Icon.tsx
+++ src/client/components/foundation/Icon/Icon.tsx
@@ -1,14 +1,23 @@
 import classNames from 'classnames';
 import type { FC } from 'react';
-import * as Icons from 'react-icons/fa';
+import { FaArrowLeft, FaArrowRight, FaCheckCircle, FaPlay, FaShoppingCart, FaUser } from 'react-icons/fa';
 
 import * as styles from './Icon.styles';
 
 type Props = {
-  type: keyof typeof Icons;
+  type: 'FaArrowLeft' | 'FaArrowRight' | 'FaShoppingCart' | 'FaUser' | 'FaPlay' | 'FaCheckCircle';
   width: number;
   height: number;
   color: string;
+};
+
+const Icons = {
+  FaArrowLeft,
+  FaArrowRight,
+  FaCheckCircle,
+  FaPlay,
+  FaShoppingCart,
+  FaUser,
 };
 
 export const Icon: FC<Props> = ({ color, height, type, width }) => {
