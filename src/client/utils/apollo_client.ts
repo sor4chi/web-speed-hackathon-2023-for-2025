843ab413b4b3a0ab923426ed06be3bddaf2e5fdf
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2025-02-11 13:50:47 +0900

    perf: change query option

--- src/client/utils/apollo_client.ts
+++ src/client/utils/apollo_client.ts
@@ -2,17 +2,6 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
-  defaultOptions: {
-    mutate: {
-      fetchPolicy: 'network-only',
-    },
-    query: {
-      fetchPolicy: 'network-only',
-    },
-    watchQuery: {
-      fetchPolicy: 'network-only',
-    },
-  },
-  queryDeduplication: false,
+  queryDeduplication: true,
   uri: '/graphql',
 });
