86137be0d016f72e1c16889a998d778c1c3dbd5f
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2025-02-11 13:42:22 +0900

    perf: first load review

--- src/client/hooks/useReviews.ts
+++ src/client/hooks/useReviews.ts
@@ -1,5 +1,4 @@
-import { useLazyQuery } from '@apollo/client';
-import { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
 import { useErrorHandler } from 'react-error-boundary';
 
 import type { GetProductReviewsQueryResponse } from '../graphql/queries';
@@ -8,26 +7,15 @@ import { GetProductReviewsQuery } from '../graphql/queries';
 export const useReviews = (productId: number | undefined) => {
   const handleError = useErrorHandler();
 
-  const [loadReviews, reviewsResult] = useLazyQuery<GetProductReviewsQueryResponse>(GetProductReviewsQuery, {
+  const reviewResult = useQuery<GetProductReviewsQueryResponse>(GetProductReviewsQuery, {
     onError: handleError,
+    skip: !productId,
     variables: {
       productId,
     },
   });
 
-  useEffect(() => {
-    // サーバー負荷が懸念されそうなので、リクエストを少し待つ
-    // サーバー負荷がなくなれば、すぐ読み込んでもよい
-    const timer = setTimeout(() => {
-      loadReviews();
-    }, 1000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [loadReviews, productId]);
-
-  const reviews = reviewsResult.data?.product.reviews;
+  const reviews = reviewResult.data?.product.reviews;
 
   return { reviews };
 };
